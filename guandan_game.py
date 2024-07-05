f364f3d2287ad79603ef71238e47c3f067079b9f
1EastonJ <jiangyidong9343@gmail.com>
2024-07-05 11:20:28 +0800

    Add files via upload

--- guandan_game.py
+++ guandan_game.py
@@ -2,60 +2,59 @@ from typing import List, Union, Tuple, Iterable
 
 from guandan_player import GDPlayer
 
-from utils import inRange, generateNRandomCardLists
+from utils import inRange, generateNRandomCardLists, do_move
 from numpy import random
 
+
 class GDGame(object):
-    
-    def __init__(self, players : Union[List[GDPlayer], Tuple[GDPlayer]]) -> None:
-        assert isinstance(players, Iterable)
+
+    def __init__(self, players: Union[List[GDPlayer], Tuple[GDPlayer]]) -> None:
+        assert isinstance(players, Iterable) and inRange(len(players), (2, 4))
 
         self.players = list(players)
-        self.players.sort(key = lambda p : p.my_pos)
+        self.players.sort(key=lambda p: p.my_pos)
         self.num_player = len(self.players)
-        
-        if self.num_player == 1:
-            self.players[0].my_pos = 0
-    
-    def selfplay(self) -> None:
+
+    def has_a_winner(self) -> bool:
+        for player in self.players:
+            if player.has_no_cards():
+                print(f"Player {player.my_pos} wins!")
+                return True
+        return False
+
+    def self_play(self, player) -> None:
         '''
         This function play one round of game.\n
         Call this function repeatly to play multiple rounds of game.
         '''
-        
-        # Only allow one player to play
-        if self.num_player != 1:
-            return
-        
-        nums = tuple([random.randint(1, 28), random.randint(1, 28)])
-        card_list = generateNRandomCardLists(nums)
-        
-        index = 0
-        
-        self.players[0].receive_cards(card_list[index])
-        self.players[0].receive_oppo_cards(card_list[(index + 1) % 2], 1)
-        
-        greatest_index = 0
-        greatest_action = None
-        
         game_over = False
-        
-        while not game_over:
-            action = self.players[0].make_action(greatest_action)
-            
-            if action is not None:
-                greatest_index = index
-                remaining = self.players[0].remove_cards(action[2])
-                greatest_action = action.copy()
-                card_list[index] = remaining.copy()
-                if len(remaining) == 0:
-                    game_over = True
-            
-            if game_over:
+
+        random_numbers = tuple(random.randint(1, 28, size=self.num_player))
+        card_lists = generateNRandomCardLists(random_numbers)
+
+        for i in range(self.num_player):
+            self.players[i].receive_cards(card_lists[i])
+
+        player_index = [player.my_pos for player in self.players]
+        idx = random.randint(1)
+        states, mcts_probs, current_players = [], [], []
+
+        while True:
+
+            now = self.players[idx]
+            # get_action需要写
+            move, move_probs = player.get_action(card_lists, now,
+                                                 temp=temp,
+                                                 return_prob=1)
+
+            states.append(card_lists)
+            mcts_probs.append(move_probs)
+            current_players.append(self.players[idx])
+
+            # do_move需要写
+            do_move(move, now)
+            idx += 1
+            if self.has_a_winner():
                 break
-            
-            if greatest_index != index:
-                greatest_action = None
-            index = (index + 1) % 2
-            self.players[0].receive_cards(card_list[index])
-            self.players[0].receive_oppo_cards(card_list[(index + 1) % 2], 1)
+
+        return current_players[-1], zip(states, mcts_probs, current_players)
